a16176c9a3ab1383efc5add85c370c72d889466f
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-27 13:08:15 -0300

    fix : jpa lib temporary delete(for check completed build and test) when db need import jpa

--- maplekiosk-authority/build.gradle.kts
+++ maplekiosk-authority/build.gradle.kts
@@ -19,11 +19,11 @@ repositories {
 
 dependencies {
   implementation(project(":common"))
+  implementation(project(":maplekiosk-logging"))
 
   implementation("org.springframework.boot:spring-boot-starter-web") {
     exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
   }
-  implementation("org.springframework.boot:spring-boot-starter-log4j2")
   implementation("org.springframework.boot:spring-boot-starter-security")
 
   implementation("io.jsonwebtoken:jjwt-api:0.11.5")
@@ -31,16 +31,24 @@ dependencies {
   runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
   implementation("org.springframework.boot:spring-boot-starter-validation")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  // implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
   // Lombok
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
 
   runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.3.2")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-test") {
+    exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
+  }
+}
+
+configurations.all {
+    exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
