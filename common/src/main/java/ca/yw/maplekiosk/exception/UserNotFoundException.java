7d7d292ae038827995de773fae48c7a4c5d9e992
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-28 22:56:02 -0300

    feat : custom user not found exception simple

--- common/src/main/java/ca/yw/maplekiosk/exception/UserNotFoundException.java
+++ common/src/main/java/ca/yw/maplekiosk/exception/UserNotFoundException.java
@@ -1,11 +1,10 @@
 package ca.yw.maplekiosk.exception;
 
-import ca.yw.maplekiosk.enums.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public class UserNotFoundException extends RuntimeException {
-  public UserNotFoundException(ErrorCode errorCode, Throwable cause) {
-    super(errorCode.getMessage(), cause);
+  public UserNotFoundException(String message) {
+    super(message);
   }
 }
