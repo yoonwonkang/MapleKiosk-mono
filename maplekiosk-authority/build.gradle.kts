191b56fb5eb999016f33403a81c840fd4e729abe
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-30 13:10:43 -0300

    feat : simple swagger add

--- maplekiosk-authority/build.gradle.kts
+++ maplekiosk-authority/build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-validation")
   // implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+  //swagger
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
   // Lombok
   compileOnly("org.projectlombok:lombok:1.18.32")
   annotationProcessor("org.projectlombok:lombok:1.18.32")
