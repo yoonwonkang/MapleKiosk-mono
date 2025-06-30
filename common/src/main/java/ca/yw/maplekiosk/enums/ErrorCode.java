b6f9b79e2e654f5227b0812ddc84f0019e12d164
yoonwon.kang <yoonwon.kang@gmail.com>
2025-06-29 21:00:17 -0300

    feat : errorCode style change

--- common/src/main/java/ca/yw/maplekiosk/enums/ErrorCode.java
+++ common/src/main/java/ca/yw/maplekiosk/enums/ErrorCode.java
@@ -4,13 +4,21 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-  TOKEN_EXPIRED("This token is an expired"),
-  INVALID_TOKEN("This token is not valid"),
-  UNKNOWN_ERROR("An unexpected error occured token verification ");
+  USER_NOT_FOUND("AUTH", "001", "사용자를 찾을 수 없습니다."),
+  INVALID_PASSWORD("AUTH", "002", "비밀번호가 일치하지 않습니다."),
+  TOKEN_EXPIRED("AUTH", "003", "토큰이 만료되었습니다."),
+  INVALID_TOKEN("AUTH", "004", "토큰이 유효하지 않습니다.");
 
+  private final String service;
+  private final String code;
   private final String message;
 
-  ErrorCode(String message) {
-      this.message = message;
+  ErrorCode(String service, String code, String message) {
+    this.service = service;
+    this.code = code;
+    this.message = message;
+  }
+  public String getFullCode() {
+      return service + "-" + code;
   }
 }
