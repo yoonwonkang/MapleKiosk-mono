f3c10ee2a0a85eb0ee09ea642ee97fd193aeea1a
yoonwon.kang <yoonwon.kang@gmail.com>
2025-07-01 19:50:34 -0300

    fix : unused import remove

--- maplekiosk-authority/src/test/java/ca/yw/maplekiosk/MaplekioskApplicationTests.java
+++ maplekiosk-authority/src/test/java/ca/yw/maplekiosk/MaplekioskApplicationTests.java
@@ -1,10 +1,6 @@
 package ca.yw.maplekiosk;
 
-import org.springframework.test.context.ActiveProfiles;
-
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 // @SpringBootTest
 // @ActiveProfiles("test")
