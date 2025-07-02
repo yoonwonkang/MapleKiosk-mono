bd176e5940e97a7dbfd99b9db040913adcebc98f
yoonwon.kang <yoonwon.kang@gmail.com>
2025-07-02 20:20:07 -0300

    feat : fromString, isvalid add at tokentype

--- common/src/main/java/ca/yw/maplekiosk/enums/TokenType.java
+++ common/src/main/java/ca/yw/maplekiosk/enums/TokenType.java
@@ -1,5 +1,25 @@
 package ca.yw.maplekiosk.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TokenType {
-  SHOP, KIOSK
+  SHOP, KIOSK;
+
+  private static final Map<String, TokenType> TYPE_MAP = new HashMap<>();
+
+  static {
+    for (TokenType tokenType : values()) {
+      TYPE_MAP.put(tokenType.name().toLowerCase(), tokenType);
+    }
+  }
+
+  public static TokenType fromString(String type) {
+    if (type == null) return null;
+    return TYPE_MAP.get(type.toLowerCase());
+  }
+
+  public static boolean isValid(String type) {
+    return !(TokenType.fromString(type) == null);
+  }
 }
