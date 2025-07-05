1b3576e418a664dffe7aecc71532b7bfd2cc9105
yoonwon.kang <yoonwon.kang@gmail.com>
2025-07-05 16:41:17 -0300

    feat : valid catch handler add

--- maplekiosk-authority/src/main/java/ca/yw/maplekiosk/handler/GlobarExceptionHandler.java
+++ maplekiosk-authority/src/main/java/ca/yw/maplekiosk/handler/GlobarExceptionHandler.java
@@ -3,10 +3,12 @@ package ca.yw.maplekiosk.handler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import ca.yw.maplekiosk.dto.common.ErrorResponse;
+import ca.yw.maplekiosk.enums.ErrorCode;
 import ca.yw.maplekiosk.exception.AuthException;
 
 
@@ -19,12 +21,17 @@ public class GlobarExceptionHandler {
   public ResponseEntity<?> handleJwtTokenException(AuthException e) {
     log.error("JWT Exception: {}", e.getMessage());
     return ResponseEntity
-            .status(e.getStatus())
-            .body(
-              ErrorResponse.builder()
-              .code(e.getErrorCode().name())
-              .message(e.getMessage())
-              .build()
-            );
+      .status(e.getStatus())
+      .body(
+        ErrorResponse.builder()
+        .code(e.getErrorCode().name())
+        .message(e.getMessage())
+        .build()
+      );
+  }
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+      String message = ex.getMessage();
+      return ResponseEntity.badRequest().body(ErrorResponse.builder().code(ErrorCode.INVALID_REQUEST.name()).message(message).build());
   }
 }
