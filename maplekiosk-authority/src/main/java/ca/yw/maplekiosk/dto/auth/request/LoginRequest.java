c5b1a3f767cd2a0f8242c743d966efe9494f6283
yoonwon.kang <yoonwon.kang@gmail.com>
2025-07-05 16:40:56 -0300

    feat : dto add simple valid

--- maplekiosk-authority/src/main/java/ca/yw/maplekiosk/dto/auth/request/LoginRequest.java
+++ maplekiosk-authority/src/main/java/ca/yw/maplekiosk/dto/auth/request/LoginRequest.java
@@ -1,5 +1,6 @@
 package ca.yw.maplekiosk.dto.auth.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+  @NotBlank(message = "Username must not be empty")
   private String username;
+  @NotBlank(message = "Password must not be empty")
   private String password;
 }
