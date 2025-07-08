ee2fd7f9389a1405ce5a4416b76da6f18499e8e6
yoonwon.kang <yoonwon.kang@gmail.com>
2025-07-08 16:02:55 -0300

    test : logout test

--- maplekiosk-authority/src/test/java/ca/yw/maplekiosk/service/AuthIntegrationServiceTest.java
+++ maplekiosk-authority/src/test/java/ca/yw/maplekiosk/service/AuthIntegrationServiceTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+import java.sql.Date;
+import java.time.Instant;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,7 +14,12 @@ import org.junit.jupiter.api.Test;
 import ca.yw.maplekiosk.dto.auth.request.LoginRequest;
 import ca.yw.maplekiosk.enums.ErrorCode;
 import ca.yw.maplekiosk.exception.AuthException;
+import ca.yw.maplekiosk.model.token.TokenBlackList;
+import ca.yw.maplekiosk.model.token.TokenBlackListRepository;
+import ca.yw.maplekiosk.provider.JwtTokenProvider;
 import ca.yw.maplekiosk.service.login.LoginService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 
 public class AuthIntegrationServiceTest {
 
@@ -53,5 +60,54 @@ public class AuthIntegrationServiceTest {
     assertThatThrownBy(() -> authIntegrationService.login(invalidType, loginRequest))
       .isInstanceOf(AuthException.class)
       .hasMessageContaining(ErrorCode.INVALID_USER_TYPE.getMessage());
+  }
+
+  @Test
+  @DisplayName("logout_success")
+  void logout_success() {
+    JwtTokenProvider jwtTokenProvider = mock(JwtTokenProvider.class);
+    TokenBlackListRepository tokenBlackListRepository = mock(TokenBlackListRepository.class);
+    AuthIntegrationService authIntegrationService = new AuthIntegrationService(null, jwtTokenProvider, tokenBlackListRepository);
+
+    String token = "valid-token";
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    Claims claims = mock(Claims.class);
+
+    when(jwtTokenProvider.resolveToken(mockRequest)).thenReturn(token);
+    doNothing().when(jwtTokenProvider).validateToken(token);
+    when(jwtTokenProvider.getClaims(token)).thenReturn(claims);
+
+    when(claims.getExpiration()).thenReturn(Date.from(Instant.now().plusSeconds(3600)));
+    when(claims.getId()).thenReturn("123");
+    when(claims.get("role")).thenReturn("KIOSK");
+
+    // When
+    authIntegrationService.logout(mockRequest);
+
+    // Then
+    verify(jwtTokenProvider, times(1)).validateToken(token);
+    verify(tokenBlackListRepository, times(1)).save(any(TokenBlackList.class));
+  }
+
+
+  @Test
+  @DisplayName("logout_should_fail_when_token_is_invalid")
+  void logout_should_fail_when_token_is_invalid() {
+    JwtTokenProvider jwtTokenProvider = mock(JwtTokenProvider.class);
+    TokenBlackListRepository tokenBlackListRepository = mock(TokenBlackListRepository.class);
+    AuthIntegrationService authIntegrationService = new AuthIntegrationService(null, jwtTokenProvider, tokenBlackListRepository);
+
+    String token = "invalid-token";
+    HttpServletRequest request = mock(HttpServletRequest.class);
+
+    when(jwtTokenProvider.resolveToken(request)).thenReturn(token);
+    doThrow(new RuntimeException("Invalid Token")).when(jwtTokenProvider).validateToken(token);
+
+    // Then
+    assertThatThrownBy(() -> authIntegrationService.logout(request))
+      .isInstanceOf(RuntimeException.class)
+      .hasMessageContaining("Invalid Token");
+
+    verify(tokenBlackListRepository, never()).save(any());
   }
 }
